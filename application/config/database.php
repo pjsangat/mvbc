cb35deae2afa581eea5dfc73ce14ab97a9b7c6fd
Asdadasa <asdasas>
2021-09-18 00:56:11 +0800

    update

---
+++ application/config/database.php
@@ -0,0 +1,15 @@
+<?php 
+ 
+return [ 
+    'default-connection' => 'concrete', 
+    'connections' => [ 
+        'concrete' => [ 
+            'driver' => 'c5_pdo_mysql', 
+            'server' => 'localhost', 
+            'database' => 'mvbc_db', 
+            'username' => 'root', 
+            'password' => '5w0rdf15h', 
+            'charset' => 'utf8', 
+        ], 
+    ], 
+];
